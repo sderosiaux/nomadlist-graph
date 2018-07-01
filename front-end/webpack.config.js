eb7a685bea62a362470be8557eba0687f275c909
Stéphane Derosiaux <derste@gmail.com>
2018-07-02 00:09:48 +0200

    Refactor

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path')
 module.exports = {
   entry: path.join(__dirname, 'index.js'),
   output: {
-    path: path.join(__dirname),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
   }
 }
